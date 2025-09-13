f547465078add12b0cb417a135ede438c7d00a94
Msun-ny <suhyun224@gmail.com>
2025-09-14 00:19:59 +0900

    feat: router 추가가

---
+++ src/router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import NotFound from "./pages/NotFound";
+import HomePage from "./pages/Home/HomePage";
+import VideoCounselingPage from "./pages/VideoCounseling/VideoCounselingPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <HomePage /> },
+      //   { path: "reports", element: <ReportsPage /> },
+      //   { path: "missions", element: <MissionsPage /> },
+      //   { path: "notices", element: <NoticesPage /> },
+      { path: "video-counseling", element: <VideoCounselingPage /> },
+    ],
+  },
+]);
