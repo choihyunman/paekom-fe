3b9c22368e6795d5110c8c1c96d1211bde9f8b40
Msun-ny <suhyun224@gmail.com>
2025-09-14 22:21:47 +0900

    feat: 라우터에 report 관련 추가가

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,8 @@ import AppLayout from "./layouts/AppLayout";
 import NotFound from "./pages/NotFound";
 import HomePage from "./pages/Home/HomePage";
 import VideoCounselingPage from "./pages/VideoCounseling/VideoCounselingPage";
+import ReportsPage from "./pages/Report/ReportsPage";
+import ReportDetailPage from "./pages/Report/ReportDetailPage";
 
 export const router = createBrowserRouter([
   {
@@ -11,7 +13,8 @@ export const router = createBrowserRouter([
     errorElement: <NotFound />,
     children: [
       { index: true, element: <HomePage /> },
-      //   { path: "reports", element: <ReportsPage /> },
+      { path: "reports", element: <ReportsPage /> },
+      { path: "reports/:id", element: <ReportDetailPage /> },
       //   { path: "missions", element: <MissionsPage /> },
       //   { path: "notices", element: <NoticesPage /> },
       { path: "video-counseling", element: <VideoCounselingPage /> },
